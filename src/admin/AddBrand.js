6c6a73808c9b760dae0ecc20b044e81e37541906
MnqobiSB <amandlamm1@gmail.com>
2021-04-10 10:59:58 +0200

    Brand create success and error

--- src/admin/AddBrand.js
+++ src/admin/AddBrand.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import { createBrand } from './apiAdmin';
 
 const AddBrand = () => {
 	const [ name, setName ] = useState('');
@@ -21,6 +22,14 @@ const AddBrand = () => {
 		setError('');
 		setSuccess(false);
 		// make request to api to create category
+		createBrand(user._id, token, { name }).then((data) => {
+			if (data.error) {
+				setError(true);
+			} else {
+				setError('');
+				setSuccess(true);
+			}
+		});
 	};
 
 	const newBrandForm = () => (
@@ -39,6 +48,26 @@ const AddBrand = () => {
 		</form>
 	);
 
+	const showSuccess = () => {
+		if (success) {
+			return <h3 className="text-success">{name} is created</h3>;
+		}
+	};
+
+	const showError = () => {
+		if (error) {
+			return <h3 className="text-danger">Brand should be unique</h3>;
+		}
+	};
+
+	const goBack = () => (
+		<div className="mt-5">
+			<Link to="/admin/dashboard" className="text-warning">
+				Back to Dashhboard
+			</Link>
+		</div>
+	);
+
 	return (
 		<Layout
 			title="Add a new brand"
@@ -47,7 +76,12 @@ const AddBrand = () => {
 			className="container"
 		>
 			<div className="row">
-				<div className="col-md-8 offset-md-2">{newBrandForm()}</div>
+				<div className="col-md-8 offset-md-2">
+					{showSuccess()}
+					{showError()}
+					{newBrandForm()}
+					{goBack()}
+				</div>
 			</div>
 		</Layout>
 	);
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -18,3 +18,22 @@ export const createCategory = (userId, token, category) => {
 			console.log(err);
 		});
 };
+
+export const createBrand = (userId, token, brand) => {
+	// console.log(name, email, password);
+	return fetch(`${API}/brand/create/${userId}`, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`
+		},
+		body: JSON.stringify(brand)
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
