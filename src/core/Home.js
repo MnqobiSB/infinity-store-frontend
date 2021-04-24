0466eb1a4f90f1a3ca3adfce7b56a26616f4f4df
MnqobiSB <amandlamm1@gmail.com>
2021-04-24 09:44:44 +0200

    Products by arrival and sell

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getProducts } from './apiCore';
 
-const Home = () => (
-	<Layout title="Home Page" description="Infinity Online Store">
-		...
-	</Layout>
-);
+const Home = () => {
+	const [ productsBySell, setProductsBySell ] = useState([]);
+	const [ productsByArrival, setProductsByArrival ] = useState([]);
+	const [ error, setError ] = useState(false);
+
+	const loadProductsBySell = () => {
+		getProducts('sold').then((data) => {
+			if (data.error) {
+				setError(data.error);
+			} else {
+				setProductsBySell(data);
+			}
+		});
+	};
+
+	const loadProductsByArrival = () => {
+		getProducts('createdAt').then((data) => {
+			console.log(data);
+			if (data.error) {
+				setError(data.error);
+			} else {
+				setProductsByArrival(data);
+			}
+		});
+	};
+
+	useEffect(() => {
+		loadProductsByArrival();
+		loadProductsBySell();
+	}, []);
+
+	return (
+		<Layout title="Home Page" description="Infinity Online Store">
+			{JSON.stringify(productsBySell)}
+			<hr />
+			{JSON.stringify(productsByArrival)}
+		</Layout>
+	);
+};
 
 export default Home;
